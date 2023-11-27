f3ac8a46f00f77c9b51f5695ff57395e00f64a11
Claudio C. Dias <claudiocdadm@hotmail.com>
2023-11-27 15:22:18 -0300

    Inclui o Bom dias!!! no começo da impressão de texto no Console

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -9,8 +9,14 @@ public class Medidas {
 
     // 3.2 - Métodos e Funções
     public static void main(String[] args){
-        System.out.println("Bom dia!!!");
         System.out.println("Cálculo de Areas");
+
+        // Calculo de área - Reduzido
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de " + largura * comprimento + "m²");
     }
 
     public void calcularAreaModoExtenso(){
